c7c59a16923c659d2b47a5f80defba0f5093f259
ssimsir <s_simsir@yaho.com>
2023-12-22 22:10:37 +0300

    22.12.2023 22:10 commit

--- src/main/java/com/rentacar/webApi/controllers/BrandsController.java
+++ src/main/java/com/rentacar/webApi/controllers/BrandsController.java
@@ -4,15 +4,13 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rentacar.business.abstracts.BrandService;
 import com.rentacar.business.requests.CreateBrandRequest;
 import com.rentacar.business.responses.GetAllBrandsResponses;
-import com.rentacar.entities.concretes.Brand;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController   //RestController
 @RequestMapping("api/brands")
