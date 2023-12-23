004e97f840863a97fbe617f1f0a741a52b79179f
ssimsir <s_simsir@yaho.com>
2023-12-23 09:14:23 +0300

    23.12.2023 09.14 added brand update and delete

---
+++ src/main/java/com/rentacar/business/responses/GetByIdBrandResponse.java
@@ -0,0 +1,13 @@
+package com.rentacar.business.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetByIdBrandResponse {
+	private int Id;
+	private String name;
+}
